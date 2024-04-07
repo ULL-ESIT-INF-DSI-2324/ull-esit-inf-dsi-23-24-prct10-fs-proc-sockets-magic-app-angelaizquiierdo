d237d5ae836a10592aa161807a1beccb9d3dd1f2
Ángela Izquierdo´ <alu0101480442@ull.edu.es>
2024-04-07 21:02:31 +0100

    subir

---
+++ src/Ejerciciosejemplo/cliente.ts
@@ -0,0 +1,16 @@
+import { connect } from "net";
+import { MessageEventEmitterClient } from "./eventEmitter.js";
+
+const client = new MessageEventEmitterClient(connect({ port: 60300 }));
+
+client.on("message", (message) => {
+  if (message.type === "watch") {
+    console.log(`Connection established: watching file ${message.file}`);
+  } else if (message.type === "change") {
+    console.log("File has been modified.");
+    console.log(`Previous size: ${message.prevSize}`);
+    console.log(`Current size: ${message.currSize}`);
+  } else {
+    console.log(`Message type ${message.type} is not valid`);
+  }
+});
---
+++ tests/eventEmitter.spec.ts
@@ -0,0 +1,20 @@
+import 'mocha';
+import {expect} from 'chai';
+import {EventEmitter} from 'events';
+import {MessageEventEmitterClient} from '../src/Ejerciciosejemplo/eventEmitter.js';
+
+describe('MessageEventEmitterClient', () => {
+  it('Should emit a message event once it gets a complete message', (done) => {
+    const socket = new EventEmitter();
+    const client = new MessageEventEmitterClient(socket);
+
+    client.on('message', (message) => {
+      expect(message).to.be.eql({'type': 'change', 'prev': 13, 'curr': 26});
+      done();
+    });
+
+    socket.emit('data', '{"type": "change", "prev": 13');
+    socket.emit('data', ', "curr": 26}');
+    socket.emit('data', '\n');
+  });
+});
